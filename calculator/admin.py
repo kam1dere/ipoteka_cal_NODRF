5abfd5124730a71bd28076772f6ff149100809c7
kam1dere <kam1dere@yandex.ru>
2022-06-29 15:24:39 +0300

    Добавил модель

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bank
 
-# Register your models here.
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    pass
--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Bank(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование банка')
+    term_min = models.PositiveIntegerField(verbose_name='Срок ипотеки, ОТ')
+    term_max = models.PositiveIntegerField(verbose_name='Срок ипотеки, ДО')
+    rate_min = models.PositiveSmallIntegerField(verbose_name='Ставка, ОТ')
+    rate_max = models.PositiveSmallIntegerField(verbose_name='Ставка, ДО')
+    payment_min = models.PositiveIntegerField(verbose_name='Сумма кредита, ОТ')
+    payment_max = models.PositiveIntegerField(verbose_name='Сумма кредита, ДО')
+
+    def __str__(self):
+        return self.name
+
+
+'''''''''
+Можно добавить:
+Цель ипотеки
+подтверждение дохода
+Обеспечение
+Возраст заемщика и т.д.
+Но для данных целей данных полей достаточно
+
+'''''''''
