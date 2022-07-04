6722f5bf9a71ce27c6c773dc7cc68c988c2f302f
kam1dere <kam1dere@yandex.ru>
2022-07-04 14:37:01 +0300

    посмотрел гет запрос

--- calculator/forms.py
+++ calculator/forms.py
@@ -2,8 +2,7 @@ from django import forms
 
 
 class UrlForm(forms.Form):
-    url = forms.CharField(label='Введи ЮРЛ', max_length=100)
+    url = forms.CharField(label='первичный взнос', max_length=100, required=False)
+    second = forms.CharField(label='ставка', max_length=100, required=False)
+    term = forms.CharField(label='сумма', max_length=100, required=False)
 
-    def newURL(self):
-        new_url = self.cleaned_data['url']
-        return new_url
--- calculator/views.py
+++ calculator/views.py
@@ -4,7 +4,5 @@ from .forms import UrlForm
 
 def my_view(request):
     if request.method == 'GET':
-        form = UrlForm(request.POST)
-        if form.is_valid():
-            name = form.newURL
-            return redirect('')
+        form = UrlForm(request.GET)
+        return render(request, 'redir.html', {'form': form})
