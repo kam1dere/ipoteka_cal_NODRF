e68cd19b51c1693d799037cf90cbc6573a872b2b
kam1dere <kam1dere@yandex.ru>
2022-07-04 14:46:51 +0300

    установил библиотеки

--- ipoteka/settings.py
+++ ipoteka/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Сторонние библиотеки
     'rest_framework',
+    'django_filters',
     # Мои приложения
     'calculator.apps.CalculatorConfig',
 ]
@@ -126,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        ...
+    ),
+}
